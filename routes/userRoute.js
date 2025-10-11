b784c7d745ed9ec2418889c9420116d515bee79b
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-11 19:19:04 +0530

    import  functions in conroller

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { getAllUsers, getUserById } from "../controllers/userController.js";
+import {
+  createUser,
+  getAllUsers,
+  deleteUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/:id", getAllUsers);
-router.get("/:id", getUserById);
+router.get("/", getAllUsers);
+router.delete("/:id", deleteUser);
+router.post("/", createUser);
 
 export default router;
