d3994450fcd36da78c342fedb3697f024bfec429
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-11 19:18:09 +0530

    createUser function added

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,32 +7,43 @@ export const getAllUsers = async (req, res) => {
     const users = await User.find();
     res.json(users);
   } catch (error) {
-    res.error.status(500).json({ error: error });
-  }
-};
-
-export const getUserById = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const users = await User.findByIdAndDelete(userId);
-    if (!users) return res.status(404).json({ Message: "User not found" });
-    res.send(users);
-  } catch (error) {
-    res.error.status(500).json({ error: error });
+    res.status(500).json({ error: error });
   }
 };
 
 export const createUser = async (req, res) => {
   try {
-    const newUser = req.body;
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
 
-    const emailCheck = users.find((user) => user.email === email);
-    if (emailCheck)
-      return res.status(404).json({ error: "Email already exit" });
-    const savedUser = await new User.save(newUser);
     res
-      .status(201)
-      .json({ Message: "User created successfully", user: savedUser });
+      .status(200)
+      .json({ Message: "User created successfully", User: savedUser });
+  } catch (error) {
+    res.status(500).json({ Error: "Internal server error" });
+  }
+};
+
+export const createBook = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+
+    const savedUser = await newUser.save();
+    res
+      .status(200)
+      .json({ Message: "user created successfully", user: savedUser });
+  } catch (error) {
+    res.status(500).json({ Error: error });
+  }
+};
+
+// delete user
+export const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const users = await User.findByIdAndDelete(userId);
+    if (!users) return res.status(404).json({ Message: "User not found" });
+    res.send(users);
   } catch (error) {
     res.error.status(500).json({ error: error });
   }
